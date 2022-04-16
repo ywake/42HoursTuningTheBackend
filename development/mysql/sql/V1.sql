ad3770903c6fd00ad6e9352a8a68ff22faab2f28
tacomeet <takumihara1226@gmail.com>
2022-04-16 22:36:20 +0900

    reduce index

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,6 +1,6 @@
 alter table session add index session_value_index(value);
 alter table record add index record_index(record_id);
-alter table record add index record_index(status);
+-- alter table record add index record_index(status);
 alter table record_comment add index record_comment_index(linked_record_id);
 alter table group_member add index group_member_index(user_id);
 alter table record_item_file add index record_item_file_index(linked_record_id);
