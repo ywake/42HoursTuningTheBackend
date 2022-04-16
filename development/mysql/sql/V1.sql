e5fca8eda619280d24e3959110ea250c2c006bb3
ywake <ywake@student.42tokyo.jp>
2022-04-16 19:14:33 +0900

    Add: index group_member, record_item_file

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,4 @@
 alter table session add index session_value_index(value);
 alter table record_comment add index record_comment_index(linked_record_id);
+alter table group_member add index group_member_index(user_id);
+alter table record_item_file add index record_item_file_index(linked_record_id);
